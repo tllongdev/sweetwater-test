3608e54605ed279df8301d5cb31957cd272486e9
Timothy Lee Long <tllongdev@gmail.com>
2020-10-15 12:25:13 -0500

    VueRouter setup

--- src/main.js
+++ src/main.js
@@ -1,16 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+import Routes from './routes';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 // Install BootstrapVue
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 // Optionally install the BootstrapVue icon components plugin
-Vue.use(IconsPlugin)
+Vue.use(IconsPlugin);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+	routes: Routes,
+	mode: 'history',
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+	render: h => h(App),
+	router: router,
+}).$mount('#app');
