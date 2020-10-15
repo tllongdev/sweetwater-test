69c727c2bb3d58e7e98ce0f15afd91599ac0736a
Timothy Lee Long <tllongdev@gmail.com>
2020-10-15 12:36:06 -0500

    Vuex store setup

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import VueRouter from 'vue-router';
 import Routes from './routes';
+import { store } from './store/store';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
@@ -23,4 +24,5 @@ const router = new VueRouter({
 new Vue({
 	render: h => h(App),
 	router: router,
+	store: store,
 }).$mount('#app');
