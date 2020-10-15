ba409cdaa36176c4c0ec38d40d4d7d84b076010d
Timothy Lee Long <tllongdev@gmail.com>
2020-10-15 13:36:36 -0500

    basic component styles and functional layout

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+ <template>
+    <div class="wrapper">
+      <Header/>
+      <div class="content"></div>
+      <Footer />
+    </div>
+</template>
+
+ <script>
+import Header from "./common/Header";
+import Footer from "./common/Footer";
+
+export default {
+  name: "Home",
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/common/Footer.vue
@@ -0,0 +1,21 @@
+ <template>
+  <footer>
+    <div></div>
+  </footer>
+</template>
+
+ <script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+.footer {
+  min-height: 60px;
+  background: lightgrey;
+  display: flex;
+  justify-content: space-between;
+  padding-top: 20px;
+}
+</style>
