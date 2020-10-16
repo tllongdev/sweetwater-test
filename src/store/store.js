88d66a048db668bb5e9399012a1548c23643ae22
Timothy Lee Long <tllongdev@gmail.com>
2020-10-16 01:02:12 -0500

    -  added SET_CART_ITEMS, ADD_CART_ITEM, REMOVE_CART_ITEM mutations to store - added setCartItems, addToCart actions to store

--- src/store/store.js
+++ src/store/store.js
@@ -9,17 +9,25 @@ export const store = new Vuex.Store({
 		cartItems: [],
 	},
 	mutations: {
-		SET_CART_ITEMS: (state, cartItems) => {
-			state.cartItems = cartItems;
+		SET_CART_ITEMS: (state, payload) => {
+			state.cartItems = payload;
+		},
+		ADD_TO_CART: (state, payload) => {
+			const item = state.cartItems.find(i => i.itemId === payload.itemId);
+			!item ? state.cartItems.push(payload) : payload.quantity++;
+		},
+		REMOVE_FROM_CART: (state, payload) => {
+			state.cartItems.filter(i => i.itemId !== payload.itemId);
 		},
 	},
-	// actions: {
-	// 	setCartItems: ({ commit }) => {
-	// 		items.getItems().then(items => {
-	// 			commit('SetCartItems', items);
-	// 		});
-	// 	},
-	// },
+	actions: {
+		setCartItems: ({ commit }) => {
+			() => commit('SET_CART_ITEMS', items);
+		},
+		addToCart: ({ commit }, payload) => {
+			commit('ADD_TO_CART', payload);
+		},
+	},
 });
 
-(() => store.commit('SET_CART_ITEMS', items))();
+// (() => store.commit('SET_CART_ITEMS', items))();
