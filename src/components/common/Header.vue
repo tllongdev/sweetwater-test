d1620997f7fa96867045aa01bafe45dc950bb40e
Timothy Lee Long <tllongdev@gmail.com>
2020-10-16 05:31:09 -0500

    converted nav links to router methods

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -2,21 +2,22 @@
   <header>
     <div>
       <b-navbar toggleable="xl" type="light" variant="Light">
-        <b-navbar-brand href="/">
+        <b-navbar-brand @click="navToHome">
           <img
             src="https://media.sweetwater.com/api/i/f-webp__w-190__q-90__ha-f4546d53fe7e2931__hmac-a688b52eda3ea82987d2f7ba7fdad6cc81f97f4f/header/logo/sweetwater-logo.png.auto.webp"
             alt="Sweetwater"
           />
         </b-navbar-brand>
         <!-- <b-link href="/" class="ml-auto mr-3">Shop</b-link> -->
-        <b-link href="/cart">
-          <b-icon
-            icon="cart3"
-            font-scale="2"
-            class="ml-auto mr-3"
-            style="color: #4e8410"
-          ></b-icon>
-        </b-link>
+        <!-- <b-link href="/cart"> -->
+        <b-icon
+          icon="cart3"
+          font-scale="2"
+          class="ml-auto mr-3"
+          style="color: #4e8410"
+          @click="navToCart"
+        ></b-icon>
+        <!-- </b-link> -->
 
         <!-- <b-navbar-toggle target="nav-collapse"></b-navbar-toggle> -->
 
@@ -65,5 +66,13 @@
  <script>
 export default {
   name: "Header",
+  methods: {
+    navToCart() {
+      this.$router.push("/cart").catch(() => {});
+    },
+    navToHome() {
+      this.$router.push("/").catch(() => {});
+    },
+  },
 };
 </script>
